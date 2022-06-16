c295f538ca93ab5bbd5bd12c210dfadc9c5dc20f
Andrei Ozerov <andrei.ozerov@gmail.com>
2022-06-16 10:41:05 +0300

    Task #5. Operators Done

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,8 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
+    public static final double DIVIDER = 5.0;
     void divide(int toBeDivided) {
+        System.out.println(toBeDivided/DIVIDER);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,9 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
+    public static final int SECONDS = 60;
+
     public void convert(float minutes) {
+        System.out.println(minutes * SECONDS);
     }
 }
